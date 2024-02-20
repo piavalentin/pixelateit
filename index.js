af3730a8d3ec90c1d43c6ca95718c71c2837c39d
Pia Valentin <9430235+piavalentin@users.noreply.github.com>
2024-02-21 00:19:05 +0100

    chore: to do list update

--- index.js
+++ index.js
@@ -9,8 +9,9 @@ console.log(`>>> hello I am pixelateit and this is my to do list:\n
     [x] create dummy data set
     [ ] sort pixels per date
     [ ] double think about format of savings{}
-    [ ] use express?
+    [ ] PLUS use express?
     [ ] PLUS feature separate per month
+    [ ] change savings naming to neutral name
     [x] map data to color\n\n`);
 
 const yearSavings = pixelsPerYear(year);
